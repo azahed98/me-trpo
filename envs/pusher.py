14f8f509aaad7ba8a5cdc98fcdd5914863b87d7a
Arsh Zahed <arshzahed@gmail.com>
2019-06-25 16:05:41 -0700

    testing pusher

--- envs/pusher.py
+++ envs/pusher.py
@@ -30,15 +30,15 @@ class PusherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 
 
     def _step(self, a):
-        obj_pos = self.get_body_com("object")
-        vec_1 = obj_pos - self.get_body_com("tips_arm")
-        vec_2 = obj_pos - self.get_body_com("goal")
+        # obj_pos = self.get_body_com("object")
+        # vec_1 = obj_pos - self.get_body_com("tips_arm")
+        # vec_2 = obj_pos - self.get_body_com("goal")
 
-        reward_near = -np.sum(np.abs(vec_1))
-        reward_dist = -np.sum(np.abs(vec_2))
-        reward_ctrl = -np.square(a).sum()
-        reward = 1.25 * reward_dist + 0.1 * reward_ctrl + 0.5 * reward_near
-
+        # reward_near = -np.sum(np.abs(vec_1))
+        # reward_dist = -np.sum(np.abs(vec_2))
+        # reward_ctrl = -np.square(a).sum()
+        # reward = 1.25 * reward_dist + 0.1 * reward_ctrl + 0.5 * reward_near
+        reward = -1 * self.cost_np(None, a, None)
         self.do_simulation(a, self.frame_skip)
         ob = self._get_obs()
         done = False
@@ -52,12 +52,31 @@ class PusherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         self.viewer.cam.distance = 4.0
 
     def reset_model(self):
-        self.reset_mujoco(get_original_representation(init_state))
-        self.model.forward()
-        # qpos = self.init_qpos
+        qpos = self.init_qpos
 
         self.goal_pos = np.asarray([0, 0])
         self.cylinder_pos = np.array([-0.25, 0.15]) + np.random.normal(0, 0.025, [2])
+
+        qpos[-4:-2] = self.cylinder_pos
+        qpos[-2:] = self.goal_pos
+        qvel = self.init_qvel + self.np_random.uniform(low=-0.005,
+                high=0.005, size=self.model.nv)
+        qvel[-4:] = 0
+        init_state = np.vstack([qpos, qvel, np.zeros(shape=(17,1))])
+        self.reset_mujoco(init_state)
+        self.model.forward()
+        self.current_com = self.model.data.com_subtree[0]
+        self.dcom = np.zeros_like(self.current_com)
+        self.ac_goal_pos = self.get_body_com("goal")
+
+        return self._get_obs()
+
+                # self.reset_mujoco(get_original_representation(init_state))
+        # self.model.forward()
+        # # qpos = self.init_qpos
+
+        # self.goal_pos = np.asarray([0, 0])
+        # self.cylinder_pos = np.array([-0.25, 0.15]) + np.random.normal(0, 0.025, [2])
 
         # qpos[-4:-2] = self.cylinder_pos
         # qpos[-2:] = self.goal_pos
@@ -67,7 +86,7 @@ class PusherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         # self.set_state(qpos, qvel)
         # self.ac_goal_pos = self.get_body_com("goal")
 
-        return self._get_obs()
+
 
     def reset(self):
         return self.reset_model()
@@ -80,9 +99,13 @@ class PusherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
             self.get_body_com("object"),
         ])
 
+
     def get_obs(self):
         return self._get_obs()
     
+    def get_current_obs(self):
+        return self._get_obs()
+        
     def cost_np(self, x, u, x_next, ctrl_cost_coeff=.1):
         # obj_pos = self.get_body_com("object")
         # vec_1 = obj_pos - self.get_body_com("tips_arm")
